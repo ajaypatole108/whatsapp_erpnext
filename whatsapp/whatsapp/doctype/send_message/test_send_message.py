dbca1d8c19500c5b311ae581cb6939cf84cb6bd7
ajay <ajaypatole@dhuparbrothers.com>
2023-04-05 11:25:31 +0000

    doctype created

---
+++ whatsapp/whatsapp/doctype/send_message/test_send_message.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2023, Ajay Patole and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestSendMessage(FrappeTestCase):
+	pass
---
+++ whatsapp/whatsapp/doctype/whatsapp_group/whatsapp_group.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, Ajay Patole and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Whatsapp Group', {
+	// refresh: function(frm) {
+
+	// }
+});
